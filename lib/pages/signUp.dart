3e756061ff400664dfe57564fd00393eda92c18c
Sajjad Alanbaky <141661562+SajjadOmar@users.noreply.github.com>
2023-08-08 18:42:13 +0300

    Update signUp.dart

--- lib/pages/signUp.dart
+++ lib/pages/signUp.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use
+//BY @Sajjad.dev
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 
