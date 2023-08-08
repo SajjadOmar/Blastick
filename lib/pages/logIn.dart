9a6b9a9108a9a6c4343003eff115841ebee935f8
Sajjad Alanbaky <141661562+SajjadOmar@users.noreply.github.com>
2023-08-08 18:41:13 +0300

    Update logIn.dart

--- lib/pages/logIn.dart
+++ lib/pages/logIn.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+// BY @Sajjad.dev
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
